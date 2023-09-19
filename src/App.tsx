3d21da0310882ca64d0fb6b1113211ea2575fc3f
Carlos <carlos.msv@hotmail.com>
2023-09-19 00:59:22 -0300

    :lipstick: Textarea styles

--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,15 @@ export function App() {
       <main className="flex-1 p-6 flex gap-6">
         <div className="flex flex-col flex-1 gap-4">
           <div className="grid grid-rows-2 gap-4 flex-1">
-            <Textarea placeholder="Inclua o prompt para a IA..."/>
-            <Textarea placeholder="Resultado gerado pela IA..." readOnly/>
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+            />
+            <Textarea 
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA..." 
+              readOnly
+            />
           </div>
 
           <p className="text-sm text-muted-foreground">
