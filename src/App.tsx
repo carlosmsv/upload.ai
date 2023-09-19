c57d2e23ab497508ea6450ca5cfe1bcf03e63dd0
Carlos <carlos.msv@hotmail.com>
2023-09-19 00:58:12 -0300

    :sparkles: using shadcn Textarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Github } from "lucide-react"
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
 
@@ -22,6 +23,22 @@ export function App() {
             </Button>        
         </div>
       </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea placeholder="Inclua o prompt para a IA..."/>
+            <Textarea placeholder="Resultado gerado pela IA..." readOnly/>
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode usar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado
+          </p>
+        </div>
+        <aside className="w-80">
+
+        </aside>
+      </main>
     </div>
   )
 }
